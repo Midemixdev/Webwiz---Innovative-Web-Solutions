dd820e55931dd2829a5cb4629fc533115d276c1f
Midemixdev <olamidepeter912@gmail.com>
2024-08-20 03:29:22 +0100

    add

--- script.js
+++ script.js
@@ -42,24 +42,6 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroEl);
-
-// for animation for animation for animation
-
-window.addEventListener("scroll", function () {
-  const aboutMeSection = document.querySelector(".about-me-animation");
-  const observer = new IntersectionObserver(
-    (entries) => {
-      if (entries[0].isIntersecting) {
-        aboutMeSection.classList.add("animate");
-      } else {
-        aboutMeSection.classList.remove("animate");
-      }
-    },
-    { threshold: 0.5 }
-  );
-  observer.observe(aboutMeSection);
-});
-
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
@@ -68,37 +50,7 @@ btnNavEl.addEventListener("click", function () {
 
 // Remove nav-open class on page load
 headerEl.classList.remove("nav-open");
-
-// smoth smot
-
-// const allLinks = document.querySelectorAll("a:link");
-
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
-
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-
-//     // Close mobile naviagtion
-//     if (link.classList.contains("main-nav-link"))
-//       headerEl.classList.toggle("nav-open");
-//   });
-// });
-
 const allLinks = document.querySelectorAll("a:link");
-
 allLinks.forEach(function (link) {
   const href = link.getAttribute("href");
 
@@ -134,24 +86,6 @@ allLinks.forEach(function (link) {
     // No action needed, let the link navigate normally
   }
 });
-
-// const video = document.querySelector(".video");
-
-// video.addEventListener("ended", function () {
-//   video.play();
-// });
-
-// let isPlaying = false;
-// document.querySelector(".video").addEventListener("click", function () {
-//   if (!isPlaying) {
-//     this.play();
-//     isPlaying = true;
-//   } else {
-//     this.pause();
-//     isPlaying = false;
-//   }
-// });
-
 const video = document.querySelector(".video");
 
 video.addEventListener("click", () => {
